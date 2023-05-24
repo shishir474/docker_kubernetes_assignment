3e2e35c16a845bf30179396139ddf968259e7a11
Shishir Singh <shishir.s@sigmoidanalytics.com>
2023-05-24 18:42:20 +0530

    adding docker-task

---
+++ dags/docker-task.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+
+with DAG('docker-dag', start_date=datetime(2022,1,1), schedule_interval='@daily', catchup=False) as dag:
+    create_table = PostgresOperator(
+        task_id = 'create_table',
+        postgres_conn_id= 'postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS test(
+                time varchar(255) not null
+            );
+        '''
+    )
+
+    insert_execution_time = PostgresOperator(
+        task_id = 'insert_execution_time',
+        postgres_conn_id= 'postgres',
+        sql="INSERT INTO test (time) VALUES (%s);",
+        parameters=[str(datetime.now())],
+    )    
+
+    retrive_data = PostgresOperator(
+        task_id = 'retrive_data',
+        postgres_conn_id= 'postgres',
+        sql="select * from test;"
+    )    
